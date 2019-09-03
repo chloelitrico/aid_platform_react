3229620e746f306b8399720b7704d81d32b3642b
Chloe Litrico <litrico.chloe@gmail.com>
2019-09-03 10:51:52 +0200

    PrivateRoute PublicRoute

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import Login from './components/Login'
 import Navigation from './components/Navigation'
 import SignUp from './components/Signup'
+import ErrorPage from './components/ErrorPage'
+import MyMap from './components/MyMap'
 
 
 function App() {
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render = {props =>
+        sessionStorage.getItem('user') ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+            }}
+          />
+        )
+      }
+    />
+  );
+
+  const PublicRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render = {props =>
+        sessionStorage.getItem('user') ? (
+          <Redirect
+            to={{
+              pathname: "/",
+            }}
+          />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
+  );
+
   return (
     <div>
       <Navigation/>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
+        <PublicRoute path="/login" component={Login} />
+        <PublicRoute path="/signup" component={SignUp} />
+        <PrivateRoute path="/my_map" component={MyMap} />
+        <Route component={ErrorPage} />
       </Switch>
     </div>
   );
---
+++ src/components/MyMap.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class MyMap extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1> Mapmap </h1>
+      </div>
+    );
+  }
+}
+export default MyMap;
